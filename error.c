1362e13f7392d4e2bea5bc064e6075a65e64b803
therazix <therazix@gmail.com>
2020-10-16 19:51:16 +0200

    Added error codes with messages

---
+++ error.c
@@ -0,0 +1,42 @@
+/**
+ * Projekt: Implementace překladače imperativního jazyka IFJ20
+ * Autor: Filip Vágner, xvagne08
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "error.h"
+
+void print_err(int err)
+{
+	switch (err)
+	{
+		case ERR_LEXICAL:
+			fprintf(stderr, "%s\n", MSG_LEXICAL);
+			break;
+		case ERR_SYNTAX:
+			fprintf(stderr, "%s\n", MSG_SYNTAX);
+			break;
+		case ERR_SEM_DEF:
+			fprintf(stderr, "%s\n", MSG_SEM_DEF);
+			break;
+		case ERR_SEM_TYPE:
+			fprintf(stderr, "%s\n", MSG_SEM_TYPE);
+			break;
+		case ERR_SEM_COMP:
+			fprintf(stderr, "%s\n", MSG_SEM_COMP);
+			break;
+		case ERR_SEM_FUNC:
+			fprintf(stderr, "%s\n", MSG_SEM_FUNC);
+			break;
+		case ERR_SEM_OTHER:
+			fprintf(stderr, "%s\n", MSG_SEM_OTHER);
+			break;
+		case ERR_DIVBYZERO:
+			fprintf(stderr, "%s\n", MSG_DIVBYZERO);
+			break;
+		case ERR_COMPILER:
+			fprintf(stderr, "%s\n", MSG_COMPILER);
+			break;
+	}
+}
---
+++ error.h
@@ -0,0 +1,44 @@
+/**
+ * Projekt: Implementace překladače imperativního jazyka IFJ20
+ * Autor: Filip Vágner, xvagne08
+ */
+
+#ifndef ERROR_H
+#define ERROR_H
+
+#define SUCCESS 0
+
+#define ERR_LEXICAL 1
+#define MSG_LEXICAL "Lexical Analysis Error: incorrect structure of lexem"
+
+#define ERR_SYNTAX 2
+#define MSG_SYNTAX "Syntax Analysis Error: incorrect syntax of the source program / unexpected line breaks"
+
+#define ERR_SEM_DEF 3
+#define MSG_SEM_DEF "Semantic Error: undefined function or variable / trying to redefine function or variable"
+
+#define ERR_SEM_TYPE 4
+#define MSG_SEM_TYPE "Semantic Error: incorrect data type derivation of newly defined variable"
+
+#define ERR_SEM_COMP 5
+#define MSG_SEM_COMP "Semantic Error: incorrect type compatibility in expression"
+
+#define ERR_SEM_FUNC 6
+#define MSG_SEM_FUNC "Semantic Error: incorrect number/type of parameters or return values of the function"
+
+#define ERR_SEM_OTHER 7
+#define MSG_SEM_OTHER "Semantic Error"
+
+#define ERR_DIVBYZERO 9
+#define MSG_DIVBYZERO "Error: division by zero"
+
+#define ERR_COMPILER 99
+#define MSG_COMPILER "Compiler Error: internal compiler error"
+
+/**
+ * @brief Prints an error message based on the error code.
+ * @param err Error code
+ */
+void print_err(int err);
+
+#endif
